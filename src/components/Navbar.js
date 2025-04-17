2c1ef94896601125784bcaa263fa4904f98970e0
rivanov00 <r.ivanov@nisterra.bg>
2025-04-17 16:43:54 +0300

    added fontsize to navbar & prepared 2 new empty components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav style={{ padding: '1rem', background: '#282c34' }}>
-      <Link style={{ marginRight: '1rem', color: 'white' }} to="/">Home</Link>
-      <Link style={{ marginRight: '1rem', color: 'white' }} to="/login">Login</Link>
-      <Link style={{ color: 'white' }} to="/register">Register</Link>
+      <Link style={{ marginRight: '1rem', color: 'white', fontSize: '24px' }} to="/">Home</Link>
+      <Link style={{ marginRight: '1rem', color: 'white', fontSize: '24px' }} to="/login">Login</Link>
+      <Link style={{ color: 'white', fontSize: '24px' }} to="/register">Register</Link>
     </nav>
   );
 };
